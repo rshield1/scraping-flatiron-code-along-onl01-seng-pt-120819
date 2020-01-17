2a5236e4279e26aa1bff4b95393666732366192c
Robert Shields <robert.shieldsiv@gmail.com>
2020-01-17 19:45:04 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,23 @@
-
+class Course
+  attr_accessor :title, :schedule, :description
+  
+  @@all = []
+  
+  def initialize
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+ 
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
+    
+end
 
